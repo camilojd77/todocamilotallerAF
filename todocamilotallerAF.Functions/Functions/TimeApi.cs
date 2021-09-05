def8464e29f53f089aae5f3b169799c6e337f8c6
Camilo Jaramillo <camilojd77@gmail.com>
2021-09-05 14:27:01 -0500

    DeleteTime was implemented and tested.

--- todocamilotallerAF.Functions/Functions/TimeApi.cs
+++ todocamilotallerAF.Functions/Functions/TimeApi.cs
@@ -195,5 +195,37 @@ namespace todocamilotallerAF.Functions.Functions
                 Result = timeEntity
             });
         }
+
+        //---------------------------- DELETE TIME ----------------------------------------
+        [FunctionName(nameof(DeleteTime))]
+        public static async Task<IActionResult> DeleteTime(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "time/{id}")] HttpRequest req,
+           [Table("time", "TIME", "{id}", Connection = "AzureWebJobsStorage")] TimeEntity timeEntity,
+           [Table("time", Connection = "AzureWebJobsStorage")] CloudTable timeTable,
+           string id,
+           ILogger log)
+        {
+            log.LogInformation($"Delete time: {id} received.");
+
+            if (timeEntity == null)
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Time not found."
+                });
+            }
+
+            await timeTable.ExecuteAsync(TableOperation.Delete(timeEntity));
+            string message = $"Time: {timeEntity.RowKey}, deleted.";
+            log.LogInformation(message);
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = timeEntity
+            });
+        }
     }
 }
